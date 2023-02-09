0a9486e1847612b9dde0e177bf1bb1692ba59ffb
NikolaPetrasinovic <nikola.petrasinovic@dev.telnet.rs>
2023-02-09 14:25:49 +0100

    dodata equal funkcija

--- clc.js
+++ clc.js
@@ -6,6 +6,15 @@
     let clear = document.querySelector('.btn-clear')
     let equal = document.querySelector('btn-equal')
 
+    equal.addEventListener('click', function(e){
+        if(screen.value === ''){
+          screen.value = 'Please Enter a Value';
+        } else {
+          let answer = eval(screen.value);
+          screen.value = answer;
+        }
+      })
+
     clear.addEventListener('click', function(e){
         screen.value = '';
       })
