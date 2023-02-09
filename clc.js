0599978bf04d2c2a99aa4beadeccc07a6fbcbdfb
NikolaPetrasinovic <nikola.petrasinovic@dev.telnet.rs>
2023-02-09 14:28:15 +0100

    vracanje vrednosti od brojeva koji su pritisnuti

--- clc.js
+++ clc.js
@@ -6,6 +6,20 @@
     let clear = document.querySelector('.btn-clear')
     let equal = document.querySelector('btn-equal')
 
+    buttons.forEach(function(button){
+        button.addEventListener('click', function(e){
+          let value = e.target.dataset.num;
+          screen.value += value;
+        })
+      });
+
+    buttons.forEach(function(button){
+        button.addEventListener('click', function(e){
+          let value = e.target.dataset.num;
+          screen.value += value;
+        })
+      });
+
     equal.addEventListener('click', function(e){
         if(screen.value === ''){
           screen.value = 'Please Enter a Value';
