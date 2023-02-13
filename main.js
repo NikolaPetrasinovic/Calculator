9073984dd3deb5f59e09852c8e06f34b1f614dad
NikolaPetrasinovic <nikola.petrasinovic@dev.telnet.rs>
2023-02-13 13:44:16 +0100

    kreirane funkcije addEvents i updateTotal

--- main.js
+++ main.js
@@ -11,4 +11,50 @@ closeCart.addEventListener('click', ()=>{
     cart.classList.remove('active');
 })
 
+//start ako je dokument spreman
+if(document.readyState == "loading"){
+    document.addEventListener('DOMContentLoaded', start);
+}else{
+    start();
+}
 
+function start(){
+    addEvents();
+}
+
+function update(){
+    addEvents();
+    updateTotal();
+}
+
+function addEvents(){
+    //uklanjanje proizvoda iz korpe
+    let cartRemove_btns = document.querySelectorAll(".cart-remove");
+    console.log(cartRemove_btns);
+    cartRemove_btns.forEach((btn) => {
+        btn.addEventListener("click", handle_removeCartItem);
+    });
+
+    //promena kolicine proizvoda
+}
+
+//handle funckije
+function handle_removeCartItem(){
+    this.parentElement.remove();
+
+    update();
+}
+
+//update i rerender funkcije
+function updateTotal(){
+    let cartBoxes = document.querySelectorAll("cart-box");
+    const totalElement = cart.querySelector(".total-price");
+    let total = 0;
+    cartBoxes.forEach((cartBoxe) => {
+        let priceElement = cartBox.querySelector("cart-price");
+        let price = parseFloat(priceElement.innerHTML.replace("$", ""));
+        let quantity = cartBox.querySelector(".cart-quantity").value;
+        total += price * quantity;
+    });
+    totalElement.innerHTML = "$" + total;
+}
