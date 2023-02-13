6822ea660a3f9b2179f3f85fcef793d8b947cdf0
NikolaPetrasinovic <nikola.petrasinovic@dev.telnet.rs>
2023-02-13 13:18:38 +0100

    definisane konstante za korpu i kreirani listeneri za otvaranje i zatvaranje

---
+++ main.js
@@ -0,0 +1,14 @@
+//otvaranje i zatvaranje korpe
+const cartIcon = document.querySelector("#cart-icon");
+const cart = document.querySelector(".cart");
+const closeCart = document.querySelector("#cart-close");
+
+cartIcon.addEventListener('click', ()=>{
+    cart.classList.add('active');
+})
+
+closeCart.addEventListener('click', ()=>{
+    cart.classList.remove('active');
+})
+
+
